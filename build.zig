rockorager/comlink

commit 92383c2d7bf128071ab662de61a718b3e44d7b8d

chore(build): add -Dpie build option to create PIE executable

build.zig
@@ -8,6 +8,8 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const pie = b.option(bool, "pie", "Build a Position Independent Executable") orelse false;
+
     // manpages
     {
         var man_step = zzdoc.addManpageStep(b, .{
@@ -46,6 +48,8 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+    exe.pie = pie;
+
     const opts = b.addOptions();
     const version_string = version(b) catch |err| {
         std.debug.print("{}", .{err});

commit ffabed4603b521f93e224d72216a089a398b5638

build: default PIE to null
Default PIE to null, which then let's the zig compiler choose a default
value. The option is exposed to let packagers choose true or false,
otherwise default to whatever zig has chosen.

Fixes: 92383c2d7bf1
Fixes: #13

build.zig
@@ -8,7 +8,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const pie = b.option(bool, "pie", "Build a Position Independent Executable") orelse false;
+    const pie = b.option(bool, "pie", "Build a Position Independent Executable");
 
     // manpages
     {