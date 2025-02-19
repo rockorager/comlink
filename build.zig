01b1b967e4c3566d27c2b902ea64bb068d4c863d
Tim Culverhouse <tim@timculverhouse.com>
2025-02-19 11:32:39 -0600

    wip: initial vxfw compilation

--- build.zig
+++ build.zig
@@ -34,7 +34,6 @@ pub fn build(b: *std.Build) void {
     const vaxis_dep = b.dependency("vaxis", .{
         .target = target,
         .optimize = optimize,
-        .libxev = false,
     });
 
     const zeit_dep = b.dependency("zeit", .{
