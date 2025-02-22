2e738aa808b6ff55a4948087dcf08ee014481143
Tim Culverhouse <tim@timculverhouse.com>
2025-02-22 06:56:11 -0600

    ui: draw scrollbar

--- src/Scrollbar.zig
+++ src/Scrollbar.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const vaxis = @import("vaxis");
+const vxfw = vaxis.vxfw;
 
 const Scrollbar = @This();
 
@@ -10,28 +11,58 @@ character: vaxis.Cell.Character = .{ .grapheme = "▐", .width = 1 },
 style: vaxis.Style = .{},
 
 /// The index of the bottom-most item, with 0 being "at the bottom"
-bottom: usize = 0,
+bottom: u16 = 0,
 
 /// total items in the list
-total: usize,
+total: u16,
 
 /// total items that fit within the view area
-view_size: usize,
+view_size: u16,
 
-pub fn draw(self: Scrollbar, win: vaxis.Window) void {
+fn widget(self: *Scrollbar) vxfw.Widget {
+    return .{
+        .userdata = self,
+        .drawFn = drawFn,
+    };
+}
+
+fn drawFn(ptr: *anyopaque, ctx: vxfw.DrawContext) std.mem.Allocator.Error!vxfw.Surface {
+    const self: *Scrollbar = @ptrCast(@alignCast(ptr));
+    return self.draw(ctx);
+}
+
+pub fn draw(self: *Scrollbar, ctx: vxfw.DrawContext) std.mem.Allocator.Error!vxfw.Surface {
+    const max = ctx.max.size();
+    if (max.width == 0 or max.height == 0) {
+        return .{
+            .size = .{ .width = 0, .height = 0 },
+            .widget = self.widget(),
+            .buffer = &.{},
+            .children = &.{},
+        };
+    }
+
+    const surface = try vxfw.Surface.init(
+        ctx.arena,
+        self.widget(),
+        .{ .width = 2, .height = max.height },
+    );
+
     // don't draw when 0 items
-    if (self.total < 1) return;
+    if (self.total < 1) return surface;
 
     // don't draw when all items can be shown
-    if (self.view_size >= self.total) return;
+    if (self.view_size >= self.total) return surface;
 
     // (view_size / total) * window height = size of the scroll bar
-    const bar_height = @max(std.math.divCeil(usize, self.view_size * win.height, self.total) catch unreachable, 1);
+    const bar_height = @max(std.math.divCeil(usize, self.view_size * max.height, self.total) catch unreachable, 1);
 
     // The row of the last cell of the bottom of the bar
-    const bar_bottom = (win.height - 1) -| (std.math.divCeil(usize, self.bottom * win.height, self.total) catch unreachable);
+    const bar_bottom = (max.height - 1) -| (std.math.divCeil(usize, self.bottom * max.height, self.total) catch unreachable);
 
     var i: usize = 0;
     while (i <= bar_height) : (i += 1)
-        win.writeCell(0, bar_bottom -| i, .{ .char = self.character, .style = self.style });
+        surface.writeCell(0, @intCast(bar_bottom -| i), .{ .char = self.character, .style = self.style });
+
+    return surface;
 }
